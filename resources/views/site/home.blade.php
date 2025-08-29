d19b51645f3d955babcce6ce5aa94ac8b0b81736
Roni <ronielli.acicredi@sicoob.com.br>
2025-08-29 11:08:53 -0300

    ocultando o botão de visualizar caso o usuário não tenha permissão

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -9,9 +9,11 @@
                 <div class="card">
                     <div class="card-image">
                         <img src="{{ $produto->imagem }}">
-                        <a href="{{ route('produto.details', $produto->slug) }}"
-                            class="btn-floating halfway-fab waves-effect waves-light red"><i
-                                class="material-icons">visibility</i></a>
+                        @can('verProduto', $produto)
+                            <a href="{{ route('produto.details', $produto->slug) }}"
+                                class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                    class="material-icons">visibility</i></a>
+                        @endcan
                     </div>
                     <div class="card-content">
                         <span class="card-title">{{ Str::limit($produto->nome, 20) }}</span>
