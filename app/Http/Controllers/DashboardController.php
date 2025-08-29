f3c9c9ad479915c8bbad7b655234c399857287ac
Roni <ronielli.acicredi@sicoob.com.br>
2025-08-29 17:29:34 -0300

    colocando informações dinamicas nos dois gráficos com informações do controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
+use App\Models\Produto;
+use App\Models\User;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $users = User::all();
+
+        $usersData = User::select([
+            DB::raw('YEAR(created_at) as ano'),
+            DB::raw('COUNT(*) as total')
+        ])->groupBy('ano')
+            ->orderBy('ano', 'asc')
+            ->get();
+
+        foreach ($usersData as $user) {
+            $ano[] = $user->ano;
+            $total[] = $user->total;
+        }
+
+        $userLabel = "'Comparativo de cadastros de usuários'";
+        $userAno = implode(',', $ano);
+        $userTotal = implode(',', $total);
+
+        $categoriaData = Categoria::all();
+
+        foreach ($categoriaData as $cat) {
+            $catNome[] = "'" . $cat->nome . "'";
+            $catTotal[] = Produto::where('id_categoria', $cat->id)->count();
+        }
+
+        $catLabel = implode(',', $catNome);
+        $catTotal = implode(',', $catTotal);
+
+
+        return view('admin.dashboard', compact('users', 'userLabel', 'userAno', 'userTotal', 'catLabel', 'catTotal'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -59,7 +59,7 @@
                 <article class="bg-gradient-blue card z-depth-4 ">
                     <i class="material-icons">face</i>
                     <p>Usuários</p>
-                    <h3>34 </h3>
+                    <h3>{{ $users->count() }} </h3>
                 </article>
             </div>
 
@@ -93,3 +93,62 @@
 
     </div>
 @endsection
+
+@push('graficos')
+    <script>
+        /* Gráfico 01 */
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [{{ $userAno }}],
+                datasets: [{
+                    label: {!! $userLabel !!},
+                    data: [{{ $userTotal }}],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+        /* Gráfico 02 */
+        var ctx = document.getElementById('myChart2');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: [{!! $catLabel !!}],
+                datasets: [{
+                    label: 'Visitas',
+                    data: [{{ $catTotal }}],
+                    backgroundColor: [
+                        'rgba(255, 99, 132)',
+                        'rgba(54, 162, 235)',
+                        'rgba(255, 159, 64)',
+                        'rgba(54, 162, 235)',
+                        'rgba(255, 99, 132)',
+                    ]
+                }]
+            }
+        });
+    </script>
+@endpush
